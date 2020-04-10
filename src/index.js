a27918a5b41b0e87fd57848f2655c2393f76fcf6
Bhatia, Binit <47129292+bhatibi@users.noreply.github.com>
2020-04-10 13:18:30 +0530

    Initial commit. Add basic component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ItemList from './components/ItemList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+
+    <ItemList />
   </React.StrictMode>,
   document.getElementById('root')
 );
