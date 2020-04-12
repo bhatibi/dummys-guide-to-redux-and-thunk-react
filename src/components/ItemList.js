abfb971792f5810e98821684406c09077e635d71
Bhatia, Binit <47129292+bhatibi@users.noreply.github.com>
2020-04-12 15:16:02 +0530

    Step 9: Update component to make use of props.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,42 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { itemsFetchData } from '../actions/itemsActions';
 
 class ItemList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      items: []
-    };
-  }
-
-  fetchData(url) {
-    this.setState({ isLoading: true });
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        this.setState({ isLoading: false });
-        return response;
-      })
-      .then((response) => response.json())
-      .then((items) => this.setState({ items }))
-      .catch(() => this.setState({ hasErrored: true }));
-  }
-
   componentDidMount() {
-    this.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+    this.props.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
   }
 
   render() {
-    if (this.state.hasErrored) {
+    if (this.props.hasErrored) {
       return <p>Sorry! There was an error loading the items</p>;
     }
-    if (this.state.isLoading) {
+
+    if (this.props.isLoading) {
       return <p>Loading…</p>;
     }
+
     return (
       <ul>
-        {this.state.items.map((item) => (
+        {this.props.items.map((item) => (
           <li key={item.id}>
             {item.label}
           </li>
@@ -45,4 +28,36 @@ class ItemList extends Component {
     );
   }
 }
-export default ItemList;
+
+ItemList.propTypes = {
+  fetchData: PropTypes.func.isRequired,
+  items: PropTypes.array.isRequired,
+  hasErrored: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired
+};
+
+/*
+We create a function that accepts state and then returns
+an object of props. In a simple component like this, I remove
+the prefixing for the has/is props as it's obvious that they're related to items.
+*/
+const mapStateToProps = state => {
+  return {
+    items: state.items,
+    hasErrored: state.itemsHasErrored,
+    isLoading: state.itemsIsLoading
+  }
+}
+
+/*
+we need another function to be able to dispatch our
+ itemsFetchData() action creator with a prop.
+*/
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchData: (url) => dispatch(itemsFetchData(url))
+  };
+};
+
+//export default ItemList;
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
